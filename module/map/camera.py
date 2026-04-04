5dd80e6a93e9293d7ce14ee9fe7e31992ce53844
zhang huaibo <zhanghuaibo@w20898196-3.local>
2026-04-04 21:34:55 +0800

    Fix: Unify error message format in MapDetectionError

--- module/map/camera.py
+++ module/map/camera.py
@@ -117,7 +117,7 @@ class Camera(MapOperation):
                     and not self.is_in_strategy_mob_move() \
                     and not self.is_in_strategy_air_strike():
                 logger.warning('Image to detect is not in_map')
-                raise MapDetectionError('Image to detect is not_in_map')
+                raise MapDetectionError('Image to detect is not in_map')
             self.view.load(self.device.image)
         except MapDetectionError as e:
             if self.info_bar_count():
